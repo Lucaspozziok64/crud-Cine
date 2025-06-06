a13c4822af6199add0c4bfa896700df2863cc0f0
Lucasfi <lukafigueroa64@gmail.com>
2025-06-06 15:18:17 -0300

    logramos crear pelicula y guardarla en localStorage

--- js/app.js
+++ js/app.js
@@ -4,14 +4,20 @@ const crearPelicula = () => {
 
   const nuevaPelicula = new Pelicula(inputNombre.value, inputGenero.value, inputAnio.value);
   console.log(nuevaPelicula)
+  peliculasCreadas.push(nuevaPelicula)
+  guardarLocalStorage()
 }
 
+const guardarLocalStorage = () => {
+  localStorage.setItem('agendaPeliculas', JSON.stringify(peliculasCreadas));
+}
 
 //Variables
 const formularioPelicula = document.getElementById("formularioPelicula");
 const inputNombre = document.querySelector('#inputNombre');
 const inputGenero = document.querySelector('#inputGenero');
 const inputAnio = document.querySelector('#inputAño');
+const peliculasCreadas = JSON.parse(localStorage.getItem('agendaPeliculas')) || []
 
 //Manejadores de eventos
 formularioPelicula.addEventListener("submit", (e) => {
