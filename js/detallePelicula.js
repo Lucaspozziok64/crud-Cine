0954cb01814ab5485f3e73d8ec496bb5631e4e61
Lucasfi <lukafigueroa64@gmail.com>
2025-06-07 23:50:46 -0300

    logramos obtener el id de la peliculas y dirigirnos a la pagina detalle con sus caracteristicas

--- js/detallePelicula.js
+++ js/detallePelicula.js
@@ -1,2 +1,25 @@
+console.log(window.location);
 
-console.log(window.location)
+const parametroURL = new URLSearchParams(window.location.search);
+const id = parametroURL.get("cod");
+
+const peliculasCreadas = JSON.parse(localStorage.getItem("agendaPeliculas"));
+
+const peliculaBuscada = peliculasCreadas.find((pelicula) => pelicula.id === id);
+
+console.log(peliculaBuscada);
+const card = document.querySelector(".card");
+
+card.innerHTML = `<img src="${peliculaBuscada.imagen}" class="imagenPelicula" alt="imagen de pelicula ${peliculaBuscada.nombre}" />
+          <div class="card-body">
+            <button class="btn btn-tertiary d-flex justify-content-center w-100 bg-success"><i class="bi bi-skip-start-circle fs-1"></i></button>
+            <ul>
+              <li>Nombre: <strong>${peliculaBuscada.nombre}🎥</strong></li>
+              <li>Genero: <strong>${peliculaBuscada.genero}</strong></li>
+              <li>Año publicacion: <strong>${peliculaBuscada.anio}</strong></li>
+            <h5 class="card-title"></h5>
+            </ul>
+            <p class="card-text">
+              <small class="text-body-secondary">Última actualización hace 3 minutos</small>
+            </p>
+          </div>`;
